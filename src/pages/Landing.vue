aeb053b380802a8a09e9c7d779d8625d997a0ea2
Biniyam Lemma <lemmasbini@gmail.com>
2023-12-12 14:33:30 +0300

    - Install new packages - Create New components and pages to form the skeleton of the project

---
+++ src/pages/Landing.vue
@@ -0,0 +1,11 @@
+<script setup>
+
+</script>
+
+<template>
+<div>Landing</div>
+</template>
+
+<style scoped>
+
+</style>
---
+++ src/pages/Projects.vue
@@ -0,0 +1,11 @@
+<script setup>
+
+</script>
+
+<template>
+<div>Projects</div>
+</template>
+
+<style scoped>
+
+</style>
