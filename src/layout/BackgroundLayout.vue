6af47d433acc7c854d6d764e4ef80630a8f3cea5
Biniyam Lemma <lemmasbini@gmail.com>
2023-12-27 13:03:55 +0300

    - Implement `BackgroundLayout` - Add new svg - UI changes allover

---
+++ src/layout/BackgroundLayout.vue
@@ -0,0 +1,35 @@
+<script setup>
+</script>
+<template>
+  <div class="relative">
+    <div class="svg-background">
+    <div class="absolute inset-0 mx-auto w-full z-0 flex bg-white p-16">
+      <div class="relative w-full flex flex-wrap justify-center items-center">
+              <div
+                  class="w-full gradient-mask scale-75 grid-rows-7 lg:grid-rows-12 z-0 grid grid-cols-7 border bg-blue-100 opacity-[0.4] lg:grid-cols-12 lg:bg-transparent"
+              >
+                        <div
+                            v-for="index in 70"
+                            class="aspect-square border border-blue-200"
+                            :class="index % 5 ? '' : 'bg-blue-300 '"
+                            :key="index"
+                        />
+              </div>
+      </div>
+    </div>
+      <slot></slot>
+  </div>
+  </div>
+</template>
+
+<style scoped>
+.gradient-mask {
+  mask-image: radial-gradient(rgba(0, 0, 0, 0.7) 0%, transparent 100%);
+}
+
+.svg-background {
+  background-image: url("src/assets/laravel_and_vue.svg");
+  background-repeat: no-repeat;
+  background-size: cover; /* or contain depending on what you need */
+}
+</style>
